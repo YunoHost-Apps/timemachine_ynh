ea8f00ea8aa700dea1b9d2158d872afa569885c8
navanchauhan <navanchauhan@gmail.com>
2022-06-15 19:00:00 +0530

    initial changes

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,11 +5,25 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="deb1 deb2 php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
+pkg_dependencies="samba avahi-daemon"
 
 #=================================================
 # PERSONAL HELPERS
 #=================================================
+
+samba_sysadmin_setup () {
+    if [[ -d /etc/smb/smb.d ]]
+    then
+        echo "/etc/smb/smb.d exists on your filesystem."
+    else
+        mkdir -p /etc/smb/smb.d
+    fi
+}
+
+samba_sysadmin_update () {
+    # Update the smb.conf file
+    ls /etc/smb/smb.d/* | sed -e 's/^/include = /' > /etc/smb/includes.conf
+}
 
 #=================================================
 # EXPERIMENTAL HELPERS
