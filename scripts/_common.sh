2a8108522d8a5201f3cb81ce52245c0f1b6127f3
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-09-10 18:11:33 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="samba samba-common-bin avahi-daemon"
+#REMOVEME? pkg_dependencies="samba samba-common-bin avahi-daemon"
 
 #=================================================
 # PERSONAL HELPERS
